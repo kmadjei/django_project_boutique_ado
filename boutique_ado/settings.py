c1a014978540483bb3258257b354d383a1df1578
Kenneth Adjei <[kmadjei25@gmail.com]>
2021-07-25 16:45:24 -0400

    fixed settings.py for heroku deployment

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-tfk^(^y_=5)n0y@m@du*d=ug5$#4v+7wxhq+q+g3@l-b9ad0+=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['kmadjei-boutique-ado.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -117,21 +117,20 @@ LOGIN_REDIRECT_URL = '/'
 
 WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-if 'DA '
-DATABASES = {
-    'default': dj_database_url.parse('postgres://ibtxijwhahotro:fa62fae11395d16a12ddaac465dc3cbc81f8a2467092f3f9b0e4b3357c4c979f@ec2-54-147-93-73.compute-1.amazonaws.com:5432/d38qtvh5bo51bg')
-}
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
